08f3cbedaac7f249a53a814acee4494c537d3303
pratikshanhts2 <backenddevelopernhts2@gmail.com>
2024-05-05 00:29:07 +0530

    boxshadow,card,icons,map

--- Mern-Portfolio/client/src/Pages/TechStack/TechStack.js
+++ Mern-Portfolio/client/src/Pages/TechStack/TechStack.js
@@ -1,17 +1,43 @@
 import React from 'react'
 import '../TechStack/techstack.css'
+import { TechstackList } from '../../utils/TechstackList'
 
 const TechStack = () => {
   return (
     <>
         <div className='container techstack'>
-            <h2>Technologies Stack</h2>
-            <p>👉 including programming languages, frameworks, databases, front-end and back-end tools,and APIS</p>
+            <h2 className="col-12 mt-3 mb-1 text-center">Technologies Stack</h2>
+            <hr/>
+            <p className='pb-3 text-center'>
+              👉 including programming languages, frameworks, databases, front-end and back-end tools,and APIS
+            </p>
+            <div className='row card-layout'>
+              {TechstackList.map((tech)=>(
+
+                <div key={tech._id} className='col-md-3'>
+                  <div className='card-m-2'>
+                    <div className='card-content'>
+                      <div className='card-body'>
+                        <div className='media d-flex justify-content-center one-card'>
+                          <div className='align-self-center'>
+                            <tech.icon className='tech-icon' />
+                          </div>
+
+                          <div className='media-body'>
+                            <h5>{tech.name}</h5>
+                          </div>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                // <h1>{tech.name}</h1>
+              ))}
+            </div>
 
         </div>
-        <div className='row'>
 
-        </div>
     </>
   )
 }
---
+++ Mern-Portfolio/client/src/utils/TechstackList.js
@@ -0,0 +1,49 @@
+import { BsFiletypeHtml,BsFiletypeCss,BsBootstrap, BsFiletypePhp  } from 'react-icons/bs'
+import { SiJavascript,SiJquery ,SiLaravel, SiMysql } from 'react-icons/si'
+
+
+export const TechstackList =[
+
+    {
+        _id:1,
+        name:'HTML',
+        icon:BsFiletypeHtml,
+    },
+    {
+        _id:2,
+        name:'CSS',
+        icon:BsFiletypeCss,
+    },
+    {
+        _id:3,
+        name:'Bootstrap',
+        icon:BsBootstrap,
+    },
+    {
+        _id:4,
+        name:'Javascript',
+        icon:SiJavascript,
+    },
+
+    {
+        _id:5,
+        name:'Jquery',
+        icon:SiJquery,
+    },
+
+    {
+        _id:6,
+        name:'Php',
+        icon:BsFiletypePhp,
+    },
+    {
+        _id:7,
+        name:'Laravel',
+        icon:SiLaravel,
+    },
+    {
+        _id:8,
+        name:'Mysql',
+        icon:SiMysql ,
+    }
+]
