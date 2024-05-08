51ff6b4f995c0317649c0a2cc6ff04e9cf23a19e
pratikshanhts2 <backenddevelopernhts2@gmail.com>
2024-05-08 21:39:07 +0530

    changes

--- Mern-Portfolio/client/src/Pages/Contact/Contact.js
+++ Mern-Portfolio/client/src/Pages/Contact/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../Contact/Contact.css'
+import {BsLinkedin} from "react-icons/bs";
+import {BsGithub} from "react-icons/bs";
+
 const Contact = () => {
   return (
     <>
-      <div  className='contact'>
+      <div  className=' contact'>
         <div className='card card0 border-0'>
           <div className='row'>
               <div className='col-md-6 col-lg-6 col-xk-6 col-sm-12'>
@@ -14,33 +17,44 @@ const Contact = () => {
                 </div>
               </div>
 
-              <div className='col-lg-6 col-md-6'>
+              <div className='col-lg-6'>
+                <div className='card2'>
                 <div className='row'>
-                  <div className='card2'>
-                      <h6>Contact With Icons</h6>
+                    <div className='row title'>
+                  
+                        <h6>Contact With 
+                          <BsLinkedin color='blue' size={36} className='ms-2' />
+                          <BsGithub color='black' size={36} className='ms-2' />
+
+                        </h6>
+                    </div>
+                  
+
+                  <div className='row'>
+                      <div className='line' />
+                      <small className='or text-center'>OR</small>
+                      <div className='line' />
+                      
+                  </div>
+
+                  <div className='row'>
+                    <input type='text' name='name' placeholder='Enter Your Name' className='mb-3' />
+                  </div>
+
+                  <div className='row'>
+                    <input type='email' name='email' placeholder='Enter Your Email' className='mb-3' />
+                  </div>
+
+
+                  <div className='row'>
+                    <textarea type='text' name='msg' placeholder='Enter Your Message' className='mb-3' />
+                  </div>
+
+                  <div className='row'>
+                    <button className='button' type='submit'>Send Message</button>
                   </div>
                 </div>  
-
-                <div className='row'>
-                    <div className='line' />
-                    <small className='or text-center'>OR</small>
-                    <div className='line' />
-                    
                 </div>
-
-                <div className='row'>
-                  <input type='text' name='name' placeholder='Enter Your Name' className='mb-3' />
-                </div>
-
-                <div className='row'>
-                  <input type='email' name='email' placeholder='Enter Your Email' className='mb-3' />
-                </div>
-
-
-                <div className='row'>
-                  <textarea type='text' name='msg' placeholder='Enter Your Message' className='mb-3' />
-                </div>
-
               </div>
           </div>
         </div>
