424ac09ae6885c80e9313a9e9e30cad51835e04d
pratikshanhts2 <backenddevelopernhts2@gmail.com>
2024-05-11 08:29:37 +0530

    changes

--- Mern-Portfolio/server.js
+++ Mern-Portfolio/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const path =require('path')
 
 //dotenv configuration
 dotenv.config()
@@ -12,8 +13,21 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+//static files access
+app.use(express.static(path.join(__dirname,'./client/build')))
+
 //routes
+// app.get('/',(req,res)=>{
+//     res.send('<h1>Welcome to node server</h1>')
+// })
+
 app.use("/api/v1/portfolio",require("./routes/portfolioRoute"));
+
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 //port
 const PORT = process.env.PORT || 8080
